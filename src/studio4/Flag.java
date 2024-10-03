1e3a8bf76f520ca9e53498c1f5d6c5a8fd108a86
domin <domin@DominicsComputr.dhcp.wustl.edu>
2024-10-03 12:37:27 -0500

    Done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,20 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledCircle(.5,.5,.2);
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledCircle(.43,.55,.05);
+		StdDraw.filledCircle(.57,.55,.05);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(.5, .5, .49, .49);
+		StdDraw.filledCircle(.43,.55,.01);
+		StdDraw.filledCircle(.57,.55,.01);
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledRectangle(.5, .4, .1, .01);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.text(.5, .75, "I will");
+		StdDraw.text(.5, .25, "find you");
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,65 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		StdDraw.clear();
+		String shape = in.next();
+		System.out.print(shape);
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		
+		if("rectangle".equals(shape))
+		{
+			double xOne = in.nextDouble();
+			double yOne = in.nextDouble();
+			double width = xOne/2.0;
+			double height = yOne/2.0;
+			StdDraw.setPenColor(red,green,blue);
+			if(filled)
+			{
+				StdDraw.filledRectangle(xOne,yOne,width,height);
+			}
+			else
+			{
+				StdDraw.rectangle(xOne,yOne,width,height);
+			}
+			
+		}
+		if("ellipse".equals(shape))
+		{
+			double xOne = in.nextDouble();
+			double yOne = in.nextDouble();
+			double width = xOne/2.0;
+			double height = yOne/2.0;
+			StdDraw.setPenColor(red,green,blue);
+			if(filled)
+			{
+				StdDraw.filledRectangle(xOne,yOne,width,height);
+			}
+			else
+			{
+				StdDraw.rectangle(xOne,yOne,width,height);
+			}
+			
+		}
+		if("rectangle".equals(shape))
+		{
+			double xOne = in.nextDouble();
+			double yOne = in.nextDouble();
+			double width = xOne/2.0;
+			double height = yOne/2.0;
+			StdDraw.setPenColor(red,green,blue);
+			if(filled)
+			{
+				StdDraw.filledRectangle(xOne,yOne,width,height);
+			}
+			else
+			{
+				StdDraw.rectangle(xOne,yOne,width,height);
+			}
+			
+		}
 		
 	}
 }
